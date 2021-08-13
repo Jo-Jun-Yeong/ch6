a1cf58f5b5da65395e916edd1b9079ae741710e4
test1 <jojun-yeong@218.38.137.28>
2021-08-13 20:00:14 +0900

    08/ 13save

---
+++ src/Ex6_19.java
@@ -0,0 +1,30 @@
+////[6-19] 다음 코드의 실행 결과를 예측하여 적으시오.
+//public class Ex6_19 {
+//	
+//	
+//	public static void main(String[] args) {
+//		String str = "ABC123"; 
+//		System.out.println(str);
+//		str = change(str);
+//		System.out.println("After change:"+str);
+//		
+//	}
+//	
+//	public static String change(String str) {
+//		str += "456";
+//		return  str;
+//	}
+//	
+//}
+//
+////결과 예상 
+///* ABC123
+// * After change : ABC123
+// * 
+// *	change()에서 인스턴스 변수 str을 사용한게아니라 
+// *	change()의 지역변수를 사용해서 
+// *	Afer change에서도 ABC123이나온다.
+// *
+// *
+// * */
+//
